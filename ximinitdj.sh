690073cf77c29370ba31835522e57f4c81dde999
ximbesto <mashomid@gmail.com>
2015-03-05 17:41:13 +0330

    manual branch commit

---
+++ ximinitdj.sh
@@ -0,0 +1,5 @@
+cd /var/lib/opendj;./setup  --cli --propertiesFilePath opendj.properties --acceptLicense --no-prompt
+exec /usr/sbin/enable_insecure_key
+cp /tmp/opendj.sh /etc/service/opendj/run
+rm -rf /tmp/opendj.sh
+chmod +x /etc/service/opendj/run
